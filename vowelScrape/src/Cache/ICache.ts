15ef98e432b1318901a7ad304edab67f63d51742
mkgask <mmp@zsw.jp>
2022-04-24 01:08:23 +0900

    [delete] delete unnecessary files

--- vowelScrape/src/Cache/ICache.ts
+++
@@ -1,9 +0,0 @@
-interface ICache
-{
-    cache_filepath : string;
-
-    reset(cache_filepath : string) : string;
-
-    load(cache_filepath? : string) : string;
-    save(cache_data : string, cache_filepath? : string) : boolean;
-}
